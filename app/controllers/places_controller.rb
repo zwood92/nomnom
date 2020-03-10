29438bf9c745867c9d29d6b07e0783684f47041e
“Zach <z_wood@baylor.edu>
2020-03-10 18:12:10 +0000

    added user authentication fordestroy action

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places=Place.page(params[:page]).per_page(10)
@@ -31,13 +31,18 @@ class PlacesController < ApplicationController
     if @place.user != current_user
       return render plain: 'Not Allowed!', status: :forbidden 
     end
-    
+
     @place.update_attributes(place_params)
     redirect_to root_path
   end
 
   def destroy
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render plain: 'Not Allowed!', status: :forbidden
+    end
+    
     @place.destroy
     redirect_to root_path
   end
