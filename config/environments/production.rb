8075891782edee27d583d14546c6839c955af6a8
“Zach <z_wood@baylor.edu>
2020-03-13 03:58:38 +0000

    troubleshooting email functions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: '587',
@@ -8,7 +9,7 @@ Rails.application.configure do
     domain: 'heroku.com',
     enable_starttls_auto: true
   }
-  config.action_mailer.default_url_options = { host: 'nomnom-zwood.herokuapp.com'}
+  # config.action_mailer.default_url_options = { host: 'nomnom-zwood.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
