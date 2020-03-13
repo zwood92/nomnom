b9ff5e3cddd1805eec43478827e7c34e5c793648
“Zach <z_wood@baylor.edu>
2020-03-13 04:45:32 +0000

    trying still with auto emailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,12 +1,12 @@
 Rails.application.configure do
-
+  config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
+    domain: 'nomnom-zwood.herokuapp.com',
     enable_starttls_auto: true
   }
   config.action_mailer.default_url_options = { host: 'nomnom-zwood.herokuapp.com'}
